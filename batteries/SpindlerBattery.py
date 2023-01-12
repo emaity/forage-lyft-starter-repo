84b2197ca95cc82b995c68973ef68392572338f9
Emaity <matthew.ewhs@gmail.com>
2023-01-11 23:59:29 -0500

    Implemented beginning of Strategy method

---
+++ batteries/SpindlerBattery.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from Battery import Battery
+
+class SpindlerBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        self.years_to_replace = 2
+        self.service_threshold_date = last_service_date.replace(year=last_service_date.year + self.years_to_replace)
+        self.current_date = current_date
+    
+    def needs_service(self):
+        return self.service_threshold_date < self.current_date
