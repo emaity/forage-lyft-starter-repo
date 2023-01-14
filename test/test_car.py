8420a69500ddf3b872bfce2067883f8f1024defa
Emaity <matthew.ewhs@gmail.com>
2023-01-13 23:02:37 -0500

    Finished unit tests

--- test/test_car.py
+++ test/test_car.py
@@ -69,19 +69,15 @@ class TestNubbinBattery(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
         last_service_date = today.replace(year=today.year - 5)
-        current_mileage = 0
-        last_service_mileage = 0
 
-        battery = NubbinBattery(last_service_date, current_mileage, last_service_mileage)
+        battery = NubbinBattery(last_service_date, today)
         self.assertTrue(battery.needs_service())
 
     def test_battery_should_not_be_serviced(self):
         today = datetime.today().date()
         last_service_date = today.replace(year=today.year - 3)
-        current_mileage = 0
-        last_service_mileage = 0
 
-        battery = NubbinBattery(last_service_date, current_mileage, last_service_mileage)
+        battery = NubbinBattery(last_service_date, today)
         self.assertFalse(battery.needs_service())
 
 if __name__ == '__main__':
